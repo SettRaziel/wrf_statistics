6d5ea3f2304b413a1d25512926def63a9994a0bc
Benjamin Held <me@bheld.eu>
2017-11-02 21:17:09 +0100

    added parameter module

---
+++ lib/parameter/parameter.rb
@@ -0,0 +1,10 @@
+# @Author: Benjamin Held
+# @Date:   2017-04-17 19:41:08
+# @Last Modified by:   Benjamin Held
+# @Last Modified time: 2017-04-18 18:05:06
+
+
+module Parameter
+  require_relative 'parameter_handler'
+  require_relative 'parameter_repository'
+end
