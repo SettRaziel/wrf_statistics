50155e78745337e52dd908f59d102741c7ce70b3
Benjamin Held <me@bheld.eu>
2018-02-26 20:57:47 +0100

    added documentation to meta data

--- lib/data/meta_data/base_meta_data.rb
+++ lib/data/meta_data/base_meta_data.rb
@@ -2,19 +2,29 @@
 # @Author: Benjamin Held
 # @Date:   2018-01-27 16:45:47
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2018-02-07 21:39:48
+# @Last Modified time: 2018-02-26 20:57:04
 
+# This module holds the classes that describe the meta information for the
+# different data sets.
 module MetaData
 
+  # Abstract base class that holds the common information that all types of
+  # data sets share.
   class BaseMetaData
 
     require_relative '../coordinate'
     require_relative '../station'
     require_relative '../../string/string'
 
+    # @return [Station] the information abount the station
     attr_reader :station
+    # @return [Date] the start date of the data series
     attr_reader :start_date
 
+    # initialization
+    # @param [String] header_line the head line of a data set holding the 
+    # relevant meta information
+    # qparam [Date] start_date the start date of the data set
     def initialize(header_line, start_date)
       @start_date = start_date
       parse_header(header_line)
@@ -22,6 +32,11 @@ module MetaData
 
     private
 
+    # abstract method which parses the required meta information from the 
+    # head line    
+    # @abstract subclasses need to implement this method
+    # @param [String] header_line the head line of a data set holding the 
+    # relevant meta information
     def parse_header(header_line)
       fail NotImplementedError, " Error: the subclass #{self.class} needs " \
            "to implement the method: print_meta_information " \
