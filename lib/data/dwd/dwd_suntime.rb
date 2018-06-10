992fd1ccb6cb2faf60cf1c3b27820ec9aadbc90d
Benjamin Held <me@bheld.eu>
2018-06-10 21:17:19 +0200

    added data container for sun duration

---
+++ lib/data/dwd/dwd_suntime.rb
@@ -0,0 +1,15 @@
+# @Author: Benjamin Held
+# @Date:   2018-05-13 17:50:51
+# @Last Modified by:   Benjamin Held
+# @Last Modified time: 2018-06-10 21:16:44
+
+module Dwd
+
+  class DwdSuntime
+
+    # @return [Float] the sunshine duration in minutes per hour
+    attr_reader :sun_duration
+
+  end
+
+end
