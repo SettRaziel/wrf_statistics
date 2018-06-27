ad007cfc03f94797564ac081395f812283a6b604
Benjamin Held <me@bheld.eu>
2018-06-27 20:44:52 +0200

    corrected data creation

--- lib/data/meta_data/wrf_meta_data.rb
+++ lib/data/meta_data/wrf_meta_data.rb
@@ -1,7 +1,7 @@
 # @Author: Benjamin Held
 # @Date:   2017-11-05 20:10:11
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2018-05-06 20:45:43
+# @Last Modified time: 2018-06-27 20:44:13
 
 module MetaData
 
@@ -15,10 +15,10 @@ module MetaData
 
     def parse_header(header_line)
       entries = delete_special_chars(header_line)
-      geo_coordinate = Coordinate.new(entries[4].to_f, entries[5].to_f)
-      @grid_data = GridPoint.new(entries[6].to_f, entries[7].to_f, 
-                                 entries[8].to_f, entries[8].to_f)
-      @station = Station.new(entries[0], entries[3], entries[10], 
+      geo_coordinate = Coordinate.new(entries[5].to_f, entries[6].to_f)
+      @grid_data = GridPoint.new(entries[8].to_f, entries[9].to_f,
+                                 entries[11].to_f, entries[12].to_f)
+      @station = Station.new(entries[0], entries[3], entries[13],
                              geo_coordinate)
       nil
     end
