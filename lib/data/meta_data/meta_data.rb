37c154ebcd84f1ced839b100927c15e179d55ab4
Benjamin Held <me@bheld.eu>
2018-07-25 21:08:47 +0200

    added meta_data package file

---
+++ lib/data/meta_data/meta_data.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/ruby
+# @Author: Benjamin Held
+# @Date:   2018-01-27 16:45:47
+# @Last Modified by:   Benjamin Held
+# @Last Modified time: 2018-07-25 21:08:12
+
+module MetaData
+
+  require_relative 'base_meta_data'
+  require_relative 'wrf_meta_data'
+  require_relative 'dwd_meta_data'
+
+end  
