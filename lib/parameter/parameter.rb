07f002864455c9fc3e9241576ce253234f1e7727
Benjamin Held <me@bheld.eu>
2019-04-25 20:42:09 +0200

    adjusted lines in parameter module

--- lib/parameter/parameter.rb
+++ lib/parameter/parameter.rb
@@ -1,8 +1,7 @@
 # @Author: Benjamin Held
 # @Date:   2017-04-17 19:41:08
 # @Last Modified by:   Benjamin Held
-# @Last Modified time: 2019-04-20 17:43:42
-
+# @Last Modified time: 2019-04-25 20:40:37
 
 module Parameter
   require_relative '../wrf_library/ruby_utils/parameter/parameter'
