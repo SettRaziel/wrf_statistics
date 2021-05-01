960da19656fdbe4e6cafa2698aa5e7122ac965cb
Benjamin Held <me@bheld.eu>
2021-05-01 23:34:46 +0200

    add wrf_handler for wrf data

--- lib/wrf_statistics.rb
+++ lib/wrf_statistics.rb
@@ -1,12 +1,15 @@
+require "time"
+require "wrf_library/wrf"
 require "wrf_statistics/parameter"
 require "wrf_statistics/help/help_output"
-
 
 module WrfStatistic
 
   # Dummy class to get access to the instance variables
   class << self
 
+    # @return [Handler] the repository storing the datasets
+    attr_reader :wrf_handler
     # @return [Parameter::ParameterHandler] the handler controlling the parameters
     attr_reader :parameter_handler
     
@@ -14,6 +17,20 @@ module WrfStatistic
     # @param [Array] arguments the input values from the terminal input ARGV
     def initialize(arguments)
       @parameter_handler = Parameter::ParameterHandler.new(arguments)
+      if (!parameter_handler.repository.parameters[:help] && 
+          !parameter_handler.repository.parameters[:version])
+        initialize_wrf_handler
+      end
+    end
+
+    private
+
+    # method to initialize the wrf handler based on the available parameter
+    def initialize_wrf_handler
+      filename = @parameter_handler.repository.parameters[:file]
+      time = Time.parse(@parameter_handler.repository.parameters[:date])
+      @wrf_handler = WrfLibrary::Wrf::Handler.new(filename, time)
+      nil
     end
 
   end
